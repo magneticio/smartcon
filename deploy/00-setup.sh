464817baee7f20b2058eae07beed33e274d3360e
Vincent Lesierse <vincent@lesierse.com>
2019-05-09 15:47:55 +0200

    Change deployment scenarios

--- deploy/00-setup.sh
+++ deploy/00-setup.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 # Install Vamp Kubist CLI
-#curl -Lo vampctl https://github.com/magneticio/vampkubistcli/releases/download/v0.0.25/vamp-darwin-x86_64 && chmod +x vampctl && sudo mv vampctl /usr/local/bin/
+#curl -Lo vampctl https://github.com/magneticio/vampkubistcli/releases/download/v0.0.28/vamp-darwin-x86_64 && chmod +x vampctl && sudo mv vampctl /usr/local/bin/
 
 # Install Vamp
 vampctl install --configuration ./00-setup/vamp-configuration.yaml
-
-# Login to Vamp
-vampctl login --url https://35.204.144.201:8888 --user root --cert certificate.crt
 
 
 # Create Kubernetes Namespace
--- deploy/02-update-app.sh
+++ deploy/02-update-app.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
-
-PROJECT_NAME=${1:-vamp}
-CLUSTER_NAME=${2:-demo}
-NAMESPACE_NAME=${3:-$PROJECT_NAME}
-
-vampctl config set -p $PROJECT_NAME
-vampctl config set -c $CLUSTER_NAME
-vampctl config set -r $NAMESPACE_NAME
+vampctl config set -r smartcon
 
 kubectl apply -f ./02-update-app/k8s-deployment.yaml
 vampctl update destination smartcon-app -f ./02-update-app/vamp-destination.yaml
 vampctl update vamp_service smartcon-app -f ./02-update-app/vamp-service.yaml --host smartcon.kubecon.demo.vamp.cloud
 
-vamp release smartcon-app --destination smartcon-app --subset smartcon-app-v2 -l version=v2.0
+vampctl release smartcon-app --destination smartcon-app --port 5000 --subset smartcon-app-v2 -l version=v2.0 --type health
 
